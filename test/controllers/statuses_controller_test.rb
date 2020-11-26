cfcdd3e722e0d5b2055a84b0ee24e635e237cc2f
kentaro73 <k.uneune0703@gmail.com>
2020-11-26 13:35:49 +0900

    first commit

---
+++ test/controllers/statuses_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StatusesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get statuses_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get statuses_new_url
+    assert_response :success
+  end
+
+end
